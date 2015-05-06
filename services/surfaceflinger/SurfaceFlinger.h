6fe6a92ffb91ea33189bd8c2f523de066b066f36
wzq <wzq@rock-chips.com>
2015-05-06 15:55:45 +0800

    Add an thread for repaint when static screen

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -160,6 +160,11 @@ public:
 	Mutex  mCaptureScreenLock;
     bool ReleaseOldBuffer(void);    //rk : for lcdc composer
 
+    /* ------------------------------------------------------------------------
+    * H/W composer
+    */
+    HWComposer& getHwComposer() const { return *mHwc; }
+
 private:
     friend class Client;
     friend class DisplayEventConnection;
@@ -386,12 +391,6 @@ private:
 
     // allocate a h/w composer display id
     int32_t allocateHwcDisplayId(DisplayDevice::DisplayType type);
-
-    /* ------------------------------------------------------------------------
-     * H/W composer
-     */
-
-    HWComposer& getHwComposer() const { return *mHwc; }
 
     /* ------------------------------------------------------------------------
      * Compositing
